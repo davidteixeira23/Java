311447ef0f06b2ce020b3adae093bafaa4a9e899
David Teixeira <davidpedreira250@gmail.com>
2026-05-24 09:24:12 -0300

    Update TESTE_2.java

--- Console/Area_de_Triangulo/src/main/java/com/mycompany/teste_2/TESTE_2.java
+++ Console/Area_de_Triangulo/src/main/java/com/mycompany/teste_2/TESTE_2.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
  * @author David
  */
 public class TESTE_2 {
-123
+1234
     public static void main(String[] args) {
     
        triangulo calc = new triangulo();
