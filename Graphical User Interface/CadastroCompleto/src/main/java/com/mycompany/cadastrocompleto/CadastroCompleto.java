74d12df7c5e08212371340bf1b872b70cdc237fc
David Teixeira <davidpedreira250@gmail.com>
2026-06-10 14:31:27 -0300

    Add files via upload

---
+++ Graphical User Interface/CadastroCompleto/src/main/java/com/mycompany/cadastrocompleto/CadastroCompleto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastrocompleto;
+
+/**
+ *
+ * @author Admin
+ */
+public class CadastroCompleto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
