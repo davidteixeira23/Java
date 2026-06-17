85878ec34d16594ad4d93f0b4f2c16961732b0a8
David Teixeira <davidpedreira250@gmail.com>
2026-06-17 14:45:19 -0300

    Add files via upload

---
+++ Graphical User Interface/Exercicios_GUI/src/main/java/com/mycompany/exercicios_gui/Exercicios_GUI.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicios_gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Admin
+ */
+public class Exercicios_GUI {
+
+    public static void main(String[] args) {
+//Triangulo app = new Triangulo();
+//app.setVisible(true);
+//app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  
+   kg_em_planetas app2= new kg_em_planetas();
+   app2.setVisible(true);
+   app2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    }
+}
