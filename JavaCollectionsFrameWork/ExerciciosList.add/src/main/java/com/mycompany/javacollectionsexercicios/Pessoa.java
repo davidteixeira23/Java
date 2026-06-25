48da73d97ee928c0a53c32afd634497f78712be7
David Teixeira <davidpedreira250@gmail.com>
2026-06-25 09:49:16 -0300

    Add files via upload

---
+++ JavaCollectionsFrameWork/ExerciciosList.add/src/main/java/com/mycompany/javacollectionsexercicios/Pessoa.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javacollectionsexercicios;
+
+/**
+ *
+ * @author David
+ */
+public class Pessoa {
+    private long id;
+    private String nome;
+    
+    public Pessoa (long id, String nome) {
+    this.id = id;
+    this.nome = nome;
+        
+    }
+     
+    public String getNome() {
+    return this.nome;
+    
+    }
+    
+}
