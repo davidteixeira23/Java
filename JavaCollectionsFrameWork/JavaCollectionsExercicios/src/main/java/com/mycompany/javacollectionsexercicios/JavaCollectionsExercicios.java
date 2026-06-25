264bccb0bc23374fa20131b547fb029c36e05a2c
David Teixeira <davidpedreira250@gmail.com>
2026-06-25 09:47:47 -0300

    Add files via upload

---
+++ JavaCollectionsFrameWork/JavaCollectionsExercicios/src/main/java/com/mycompany/javacollectionsexercicios/JavaCollectionsExercicios.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javacollectionsexercicios;
+
+/**
+ *
+ * @author David
+ */
+public class JavaCollectionsExercicios {
+
+    public static void main(String[] args) {
+ExemploLIst app = new ExemploLIst();
+
+    }
+}
