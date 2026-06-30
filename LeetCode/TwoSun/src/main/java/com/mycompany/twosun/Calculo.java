c47fa6560c9894ba97988be3bcb3be74674370dc
David Teixeira <davidpedreira250@gmail.com>
2026-06-30 08:21:57 -0300

    Add files via upload

---
+++ LeetCode/TwoSun/src/main/java/com/mycompany/twosun/Calculo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.twosun;
+  import java.util.HashMap;
+/**
+ *
+ * @author David
+ */
+public class Calculo {
+    
+  
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{}; // Nunca será executado, pois o problema garante uma solução.
+    }
+}
+}
---
+++ LeetCode/TwoSun/src/main/java/com/mycompany/twosun/TwoSun.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.twosun;
+
+/**
+ *
+ * @author David
+ */
+public class TwoSun {
+
+    public static void main(String[] args) {
+Calculo calculo = new Calculo ();
+
+        }
+}
