081cf407e8e7fbbabf4e2f4905ade39c978bb9b4
David Teixeira <davidpedreira250@gmail.com>
2026-07-05 10:48:43 -0300

    Add files via upload

---
+++ JavaCollectionsFrameWork/ExerciciosList.add/ArrayList_Exercicio1Facil/src/main/java/com/mycompany/teste1/Teste1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste1;
+import java.util.*;
+/**
+ *
+ * @author David
+ */
+public class Teste1 {
+
+    public static void main(String[] args) {
+
+minhaClasse MinhaClasse = new minhaClasse();
+MinhaClasse.minhaLista();
+
+
+    }
+}
---
+++ JavaCollectionsFrameWork/ExerciciosList.add/ArrayList_Exercicio1Facil/src/main/java/com/mycompany/teste1/minhaClasse.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.teste1;
+
+// import java.util.Collections.*;
+import java.util.*;
+/**
+ *
+ * @author David
+ */
+public class minhaClasse {
+    
+    public void minhaLista() {
+    List<String> list_String = new ArrayList<String>(); {
+    list_String.add("Red");
+    list_String.add("Green");
+    list_String.add("Orange");
+    list_String.add("White");
+    list_String.add("Black");
+    System.out.println(list_String);
+    
+    
+    }
+    
+    }    
+    
+    
+    
+    
+    
+    
+}
