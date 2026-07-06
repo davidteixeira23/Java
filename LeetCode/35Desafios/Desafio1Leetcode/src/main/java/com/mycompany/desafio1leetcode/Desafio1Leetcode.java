09359ff1752bbf259d96d860b60f95ce53e9ffa7
David Teixeira <davidpedreira250@gmail.com>
2026-07-06 19:17:13 -0300

    Add files via upload

---
+++ LeetCode/35Desafios/Desafio1Leetcode/src/main/java/com/mycompany/desafio1leetcode/Desafio1Leetcode.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.desafio1leetcode;
+
+/**
+ *
+ * @author David
+ */
+public class Desafio1Leetcode {
+
+    public static void main(String[] args) {
+Contacatenacao concatenacao = new Contacatenacao();
+
+int[] nums = {1,2,3,};
+
+int[] resposta = concatenacao.getConcatenation(nums);
+ for(int numero : resposta) {
+    System.out.print(numero + "");
+ }
+        }
+}
